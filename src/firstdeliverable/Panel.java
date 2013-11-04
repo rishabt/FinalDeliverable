4b0184f7e0ab37eee42c2a743edda264941388a7
rtando1 <rtando1@open-19.CS.McGill.CA>
2013-11-04 17:59:01 -0500

    Added the basic layout for the JPanel

--- src/firstdeliverable/Panel.java
+++ src/firstdeliverable/Panel.java
@@ -1,18 +1,50 @@
+/**
+ * @author Rishabh Tandon
+ */
+
 package firstdeliverable;
 
+import java.awt.event.*;
+import java.awt.*;
 import javax.swing.*;
 
-public class Panel extends JPanel{
+public class Panel extends JPanel implements ActionListener{
 
-	public Panel(){
-		
-	}
+	private JLabel title;
+    private JLabel description;
+    private JButton start;
+    
+    public Panel(){
+            makeComponents();
+            makeLayout();
+    }
+    
 	
 	private void makeComponents(){
-		
+		title = new JLabel("Prototype Demonstration I");
+        title.setFont(new Font("Times", Font.BOLD, 37));
+        title.setBounds(310, 10, 510, 45);
+        
+        start = new JButton("Start Game");
+        start.setBounds(440, 166, 200, 60);
+        start.addActionListener(this);
+        
+        description = new JLabel("� Copyright of Group 7, ECSE 321, McGill University");
+        description.setFont(new Font("Times", Font.BOLD, 10));
+        description.setBounds(100, 500, 510, 45);
 	}
 	
 	private void makeLayout(){
+		setLayout(null);
+        add(title);
+        add(start);
+        add(description);
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 	
