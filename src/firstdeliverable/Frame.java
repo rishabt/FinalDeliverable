96fc5ad3c7cae1ef89b61156d024334f4450c6ff
rtando1 <rtando1@lab2-24.CS.McGill.CA>
2013-11-04 16:05:19 -0500

    Added the skeleton code for the JFrame that would hold the JPanel used in the prototype demonstration

---
+++ src/firstdeliverable/Frame.java
@@ -0,0 +1,18 @@
+/**
+ * @author Rishabh Tandon
+ */
+
+package firstdeliverable;
+
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Frame{
+	
+	public Frame(){
+		
+	}
+
+}
