c7429d416df75d884a5bf5f3b18d6082efa61648
rtando1 <rtando1@lab2-24.CS.McGill.CA>
2013-11-04 15:50:48 -0500

    Laid out the skeleton for the JPanel we would be using for the first prototype demonstration

---
+++ src/firstdeliverable/Panel.java
@@ -0,0 +1,19 @@
+package firstdeliverable;
+
+import javax.swing.*;
+
+public class Panel extends JPanel{
+
+	public Panel(){
+		
+	}
+	
+	private void makeComponents(){
+		
+	}
+	
+	private void makeLayout(){
+		
+	}
+	
+}
