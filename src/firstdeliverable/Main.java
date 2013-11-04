d30891b0da4a51cef5bc7903a087c2250c8a67be
rtando1 <rtando1@open-19.CS.McGill.CA>
2013-11-04 18:05:20 -0500

    Added the Main Method which would be used to run the program.

---
+++ src/firstdeliverable/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author Rishabh Tandon
+ */
+
+package firstdeliverable;
+
+import javax.swing.*;
+
+public class Main{
+        public static void main(String[] args){ 
+                Panel panel = new Panel();
+                Frame frame = new Frame();
+                Frame.addPanel(panel);
+        }
+}
